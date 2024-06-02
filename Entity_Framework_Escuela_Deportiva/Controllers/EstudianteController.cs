74c4d6c94b79a995d37be14f6fe4349c60bb96c6
CamiloSTDev <camitotena@gmail.com>
2024-06-02 14:30:17 -0500

    se agregaron las vistas de horario y perfil

---
+++ Entity_Framework_Escuela_Deportiva/Controllers/EstudianteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Entity_Framework_Escuela_Deportiva.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Entity_Framework_Escuela_Deportiva/obj/Debug/net8.0/Entity_Framework_Escuela_Deportiva.AssemblyInfo.cs
+++ Entity_Framework_Escuela_Deportiva/obj/Debug/net8.0/Entity_Framework_Escuela_Deportiva.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity_Framework_Escuela_Deportiva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97302541f783dd8d6b4964cdbc53d74e6fcaddb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa50843bdcee5c829d44b0b9300e4fe47b2ea00a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity_Framework_Escuela_Deportiva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity_Framework_Escuela_Deportiva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
