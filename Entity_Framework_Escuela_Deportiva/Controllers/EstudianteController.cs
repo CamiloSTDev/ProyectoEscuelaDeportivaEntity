e8c0ee6c6842f29c730cc039bc1f4b2977c8f819
CamiloSTDev <133534955+CamiloSTDev@users.noreply.github.com>
2024-06-03 15:34:14 -0500

    Update EstudianteController

--- Entity_Framework_Escuela_Deportiva/Controllers/EstudianteController.cs
+++ Entity_Framework_Escuela_Deportiva/Controllers/EstudianteController.cs
@@ -4,9 +4,54 @@ namespace Entity_Framework_Escuela_Deportiva.Controllers
 {
     public class EstudianteController : Controller
     {
-        public IActionResult Index()
+        private readonly EscuelaDeportivaContext _context;
+
+        public EstudianteController(EscuelaDeportivaContext context)
         {
-            return View();
+            _context = context;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Profile(int id)
+        {
+            var estudiante = await _context.Estudiantes.FindAsync(id);
+            if (estudiante == null)
+            {
+                return NotFound();
+            }
+            return View(estudiante);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateProfile(int id, [Bind("IdUsuario,Nombres,Apellidos,Doc,Telefono,FechaNac,Correo")] Estudiante estudiante)
+        {
+            if (id != estudiante.IdUsuario)
+            {
+                return BadRequest();
+            }
+
+            var estudianteToUpdate = await _context.Estudiantes.FindAsync(id);
+            if (estudianteToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            estudianteToUpdate.Nombres = estudiante.Nombres;
+            estudianteToUpdate.Apellidos = estudiante.Apellidos;
+            estudianteToUpdate.Doc = estudiante.Doc;
+            estudianteToUpdate.Telefono = estudiante.Telefono;
+            estudianteToUpdate.FechaNac = estudiante.FechaNac;
+            estudianteToUpdate.Correo = estudiante.Correo;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return BadRequest();
+            }
+
+            return RedirectToAction(nameof(Profile), new { id = estudiante.IdUsuario });
     }
 }
