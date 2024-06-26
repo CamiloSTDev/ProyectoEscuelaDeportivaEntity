1ac33ac4c84c9ba8f76fb4f7c24abde948cab9c5
KevinSRDev <kevin.s.romero.r06@gmail.com>
2024-06-25 20:39:22 -0500

    Muestra nombre de UsuarioLogueado

--- Entity_Framework_Escuela_Deportiva/obj/Debug/net8.0/Entity_Framework_Escuela_Deportiva.AssemblyInfo.cs
+++ Entity_Framework_Escuela_Deportiva/obj/Debug/net8.0/Entity_Framework_Escuela_Deportiva.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity_Framework_Escuela_Deportiva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89305f918fafa0630fe31c7203181bcb5847ccd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579be50d4bf05373d741136fcfd6b153150a436d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity_Framework_Escuela_Deportiva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity_Framework_Escuela_Deportiva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
