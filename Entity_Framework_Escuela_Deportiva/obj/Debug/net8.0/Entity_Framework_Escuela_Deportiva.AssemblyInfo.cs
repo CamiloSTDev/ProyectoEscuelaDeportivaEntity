43c955a8b76edb42e484a8b03b7959f66011c91a
CamiloSTDev <camitotena@gmail.com>
2024-06-26 08:55:23 -0500

    se actualizo el proyecto

--- Entity_Framework_Escuela_Deportiva/obj/Debug/net8.0/Entity_Framework_Escuela_Deportiva.AssemblyInfo.cs
+++ Entity_Framework_Escuela_Deportiva/obj/Debug/net8.0/Entity_Framework_Escuela_Deportiva.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity_Framework_Escuela_Deportiva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ac33ac4c84c9ba8f76fb4f7c24abde948cab9c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa8306cbade2b6ef419292b124b406c40d18092")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity_Framework_Escuela_Deportiva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity_Framework_Escuela_Deportiva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
