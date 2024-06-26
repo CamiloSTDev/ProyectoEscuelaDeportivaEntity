579be50d4bf05373d741136fcfd6b153150a436d
KevinSRDev <kevin.s.romero.r06@gmail.com>
2024-06-25 20:12:46 -0500

    Ya ejecuta sin ningun error

--- Entity_Framework_Escuela_Deportiva/Models/EscuelaDeportivaContext.cs
+++ Entity_Framework_Escuela_Deportiva/Models/EscuelaDeportivaContext.cs
@@ -35,7 +35,7 @@ public partial class EscuelaDeportivaContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=305-08\\SQLEXPRESS; database=Escuela_Deportiva; integrated security=true;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-0N9C8DO\\SQLEXPRESS;Database=Escuela_Deportiva;Integrated Security=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
